b737e0f4624520e4ba8835d3bc59da7dda8839a8
Matheus Ribeiro <matheus.ribeiro@fotosensores.com>
2021-06-26 17:50:28 -0300

    #FEAT: Lapidando código

--- servidor/models/UsuarioModel.js
+++ servidor/models/UsuarioModel.js
@@ -4,49 +4,38 @@ const Usuario = require('../entity/Usuario')
 class UsuarioModel {
 
     buscar(res, id = null) {
-        let sql
+        let sql = 'SELECT * FROM usuario'
 
-        if (id) {
-            sql = `SELECT * FROM usuario WHERE usr_id = ${id}`
-        }
-        else {
-            sql = 'SELECT * FROM usuario'
-        }
-
-        let usuarios = []
+        if(id) 
+            sql = `${sql} WHERE usr_id = ${id}`
         
-        this.query(sql, (err, resultados) => {
-            if (err) {
+        conexao.query(sql, (err, resultados) => {
+            if(err) {
                 res.status(400).send(err)
             }
             else {
-                res.status(200).send(resultados.map(usr => ({
-                        id: usr.usr_id,
-                        apelido: usr.usr_apelido
-                    })))
+                resultados = resultados.map(usr => ({
+                    id: usr.usr_id,
+                    apelido: usr.usr_apelido
+                }))
+
+                res.status(200).send((id) ? resultados[0] : resultados)
             }
         })
-
-        return usuarios
     }
 
     adicionar(dados) {
         let usuario = new Usuario(dados)
 
         const sql = 'INSERT INTO usuario SET ?'
-        
-        this.query(sql, usuario.toJSON(), (err, resultados) => {
-            if (err) {
+        conexao.query(sql, usuario.toJSON(), (err, resultados) => {
+            if(err) {
                 console.log(err)
             }
             else {
                 console.log(resultados)
             }
         })
-    }
-
-    query(sql, func, dados = []) {
-        conexao.query(sql, dados, func)
     }
 }
 
